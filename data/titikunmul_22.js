7acb91c03c2394d0b2236098ad5032a6d6ee5548
EzXens <145303127+EzXens@users.noreply.github.com>
2025-05-25 21:10:15 +0800

    Add files via upload

---
+++ data/titikunmul_22.js
@@ -0,0 +1,1 @@
+var json_titikunmul_22 = {"type":"FeatureCollection","name":"titikunmul_22","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","nama":"unmul"},"geometry":{"type":"Point","coordinates":[117.155744172798322,-0.465730842882117]}}]}
